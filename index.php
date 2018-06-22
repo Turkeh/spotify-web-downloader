0b8b78614f5a5b6ef29fb7e70987daab05a2ab89
Sami <sami@fatherbean.com>
2018-06-22 03:32:18 +0000

    Init

---
+++ index.php
@@ -0,0 +1,66 @@
+
+<!-- 
+// Form for download request
+
+	// If .network download on client, if .com download on server
+		
+		// If .com enable download location
+ -->
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Loader . Fatherbean</title>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+
+	<div class="container">
+		
+		<div class="header">
+			
+			<div class="row">
+				<div class="col-md-4">
+				</div>
+
+				<div class="col-md-8">
+				</div>
+			</div>
+
+		</div>
+
+		<div class="main">
+			
+			<div class="form-container">
+				
+			<form>
+				<div class="form-group">
+
+					<label for="spotify-link">Spotify Link</label>
+					<input type="text" class="form-control" id="spotify-link" aria-describedby="spotify-help" placeholder="Enter Link">
+					<small id="spotify-help" class="form-text text-muted">Can be the Link or URI.</small>
+
+				</div>
+				<button type="submit" class="btn btn-primary">Submit</button>
+			</form>
+
+			</div>
+
+		</div>
+
+	</div>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+
+</body>
+</html>
+
+
